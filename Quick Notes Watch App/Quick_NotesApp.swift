fd59df48a577a8bbe79197da32189478f853ceee
Nicholas Watts <148408403+Dabby-Tabby@users.noreply.github.com>
2024-09-03 17:03:01 -0500

    v1.1
    Redesigned Share Button, Animations, and App Icon.

---
+++ Quick Notes Watch App/Quick_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_NotesApp.swift
+//  Quick Notes Watch App
+//
+//  Created by Nick Watts on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Quick_Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
